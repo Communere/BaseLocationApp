36f1e039a4f5478da913c77331b79f0f1f6fd1b1
ipinlnd <nlndipi@hotmail.com>
2020-01-02 12:41:50 +0330

    Added the base backend project

---
+++ Backend/BaseLocationBackend/BaseLocationBackend/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaseLocationBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return new ObjectResult("We're up and healthy");
+        }
+    }
+}
---
+++ Backend/BaseLocationBackend/BaseLocationBackend/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BaseLocationBackend.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // TODO: Change the connection string to your own
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Locations;Integrated Security=true");
+        }
+    }
+}
