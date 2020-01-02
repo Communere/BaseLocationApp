7c9e9c3d779d9955561bef326568803d7f729812
sirwan <sir1afifi@gmail.com>
2020-01-02 13:18:36 +0330

    Add models

---
+++ backend/Models/Location.cs
@@ -0,0 +1,11 @@
+namespace backend
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        // TODO: Add Location Lat/Lon
+        // TODO: Add LocationType
+        public string Logo { get; set; }
+    }
+}
